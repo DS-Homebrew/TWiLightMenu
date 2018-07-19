9c94d3fb2f39b69c7558c8bb5719bbb971147433
Robz8 <bobesh8@gmail.com>
2018-07-19 17:58:08 -0600

    Bump version number to v5.3.1

--- titleandsettings/arm9/source/main.cpp
+++ titleandsettings/arm9/source/main.cpp
@@ -512,7 +512,7 @@ int main(int argc, char **argv) {
 	
 	char vertext[12];
 	// snprintf(vertext, sizeof(vertext), "Ver %d.%d.%d   ", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH); // Doesn't work :(
-	snprintf(vertext, sizeof(vertext), "Ver %d.%d.%d   ", 5, 3, 0);
+	snprintf(vertext, sizeof(vertext), "Ver %d.%d.%d   ", 5, 3, 1);
 
 	if (gotosettings) {
 		graphicsInit();
