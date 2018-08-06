ec07ee0956c68936260d2cd6632f70d33969bc81
Robz8 <bobesh8@gmail.com>
2018-08-05 22:40:47 -0600

    DSi/3DS theme: Improve GBA border code (still not working)

--- romsel_dsimenutheme/arm9/source/gbaswitch.cpp
+++ romsel_dsimenutheme/arm9/source/gbaswitch.cpp
@@ -1,31 +1,43 @@
 #include "gbaswitch.h"
 #include "nds_loader_arm9.h"
 
-//#include "gbaframe.h"
-//#include "gbaframe_srloader.h"
+//int gbaBorder = 1;
 
-//int gbaBorder = 0;
+/*void loadGbaBorder(const char* filename) {
+	FILE* file = fopen(filename, "rb");
+
+	if (file) {
+		// Start loading
+		fseek(file, 0xe, SEEK_SET);
+		u8 pixelStart = (u8)fgetc(file) + 0xe;
+		fseek(file, pixelStart, SEEK_SET);
+		for (int y=191; y>=0; y--) {
+			u16 buffer[256];
+			fread(buffer, 2, 0x100, file);
+			u16* src = buffer;
+			for (int i=0; i<256; i++) {
+				u16 val = *(src++);
+				VRAM_A[y*256+i] = ((val>>10)&0x1f) | ((val)&(0x1f<<5)) | (val&0x1f)<<10 | BIT(15);
+				VRAM_B[y*256+i] = ((val>>10)&0x1f) | ((val)&(0x1f<<5)) | (val&0x1f)<<10 | BIT(15);
+			}
+		}
+	}
+
+	fclose(file);
+}*/
 
 void gbaSwitch(void) {
+	irqDisable(IRQ_VBLANK);
 	vramSetBankA(VRAM_A_MAIN_BG);
 	vramSetBankB(VRAM_B_MAIN_BG);
-	/*if (gbaBorder == 2) {
-		int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
-
-		dmaCopy(gbaframe_srloaderBitmap, bgGetGfxPtr(bg3), 256*256);
-		dmaCopy(gbaframe_srloaderPal, BG_PALETTE, 256*2);
-	} else if (gbaBorder == 1) {
-		int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
-
-		dmaCopy(gbaframeBitmap, bgGetGfxPtr(bg3), 256*256);
-		dmaCopy(gbaframePal, BG_PALETTE, 256*2);
-	} else {*/
-		// Clear VRAM A and B to show black border for GBA mode
-		for (u32 i = 0; i < 0x80000; i++) {
-			*(u32*)(0x06000000+i) = 0;
-			*(u32*)(0x06200000+i) = 0;
-		}
-	//}
+	// Clear VRAM A and B to show black border for GBA mode
+	for (u32 i = 0; i < 0x80000; i++) {
+		*(u32*)(0x06000000+i) = 0;
+		*(u32*)(0x06200000+i) = 0;
+	}
+	/*if (gbaBorder == 1) {
+		loadGbaBorder("nitro:/gbaborder/nologo.bmp");
+	}*/
 	// Switch to GBA mode
 	runNdsFile ("/_nds/dsimenuplusplus/gbaswitch.srldr", 0, NULL, false);	
 }
--- romsel_dsimenutheme/arm9/source/gbaswitch.h
+++ romsel_dsimenutheme/arm9/source/gbaswitch.h
@@ -3,6 +3,14 @@
 
 #include <nds.h>
 
+#include <stdio.h>
+#include <fat.h>
+#include <sys/stat.h>
+#include <limits.h>
+
+#include <string.h>
+#include <unistd.h>
+
 extern void gbaSwitch();
 
 #endif // GBASWITCH_H
