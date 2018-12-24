ddd6e78fb097de06e4a2aa3134cdd8995af4d2f7
RocketRobz <bobesh8@gmail.com>
2018-12-24 16:14:23 -0700

    Bump version number for release

--- settings/arm9/source/settingsgui.h
+++ settings/arm9/source/settingsgui.h
@@ -18,7 +18,7 @@ public:
         _exitCallback(nullptr)
   {
 
-    snprintf(vertext, sizeof(vertext), "Ver %d.%d.%d", 6, 7, 1);
+    snprintf(vertext, sizeof(vertext), "Ver %d.%d.%d", 6, 8, 0);
 	if (isDSiMode()) {
 		// Read nds-bootstrap version
 		FILE* bsVerFile;
