465d0f5bd1fe64917ac2944245b747a1134774aa
Epicpkmn11 <41608708+Epicpkmn11@users.noreply.github.com>
2019-01-02 00:11:00 -0600

    Acekard theme: Hide `saves` folder from being shown (#304)

--- romsel_aktheme/arm9/source/windows/mainlist.cpp
+++ romsel_aktheme/arm9/source/windows/mainlist.cpp
@@ -273,7 +273,7 @@ bool MainList::enterDir(const std::string &dirName)
                     extName = "";
 
                 dbg_printf("%s: %s %s\n", (st.st_mode & S_IFDIR ? " DIR" : "FILE"), lfnBuf, extName.c_str());
-                bool showThis = (st.st_mode & S_IFDIR) ? (strcmp(lfn.c_str(), ".") && strcmp(lfn.c_str(), "..") && strcmp(lfn.c_str(), "_nds") && ms().showDirectories) : extnameFilter(extNames, extName);
+                bool showThis = (st.st_mode & S_IFDIR) ? (strcmp(lfn.c_str(), ".") && strcmp(lfn.c_str(), "..") && strcmp(lfn.c_str(), "_nds") && strcmp(lfn.c_str(), "saves") && ms().showDirectories) : extnameFilter(extNames, extName);
                 showThis = showThis && (_showAllFiles || !(attr & ATTRIB_HID));
 
                 if (showThis)
