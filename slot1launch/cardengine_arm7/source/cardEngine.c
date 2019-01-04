4dd226be33dd8de3c724c8840d5ddeb61b2d8465
RocketRobz <bobesh8@gmail.com>
2019-01-03 17:53:32 -0700

    Slot-1: Pass `hiya.dsi` as an argument for Unlaunch

--- slot1launch/cardengine_arm7/source/cardEngine.c
+++ slot1launch/cardengine_arm7/source/cardEngine.c
@@ -29,11 +29,33 @@
 #include "sr_data_srloader.h"	// For rebooting into DSiMenu++
 #include "sr_data_srllastran.h"	// For rebooting the game
 
+static const char *unlaunchAutoLoadID = "AutoLoadInfo";
+static char hiyaNdsPath[14] = {'s','d','m','c',':','/','h','i','y','a','.','d','s','i'};
+
 extern void* memcpy(const void * src0, void * dst0, int len0);	// Fixes implicit declaration
 
 extern u32 language;
 extern u32 gameSoftReset;
 static int softResetTimer = 0;
+
+static void unlaunchSetHiyaBoot(void) {
+	memcpy((u8*)0x02000800, unlaunchAutoLoadID, 12);
+	*(u16*)(0x0200080C) = 0x3F0;		// Unlaunch Length for CRC16 (fixed, must be 3F0h)
+	*(u16*)(0x0200080E) = 0;			// Unlaunch CRC16 (empty)
+	*(u32*)(0x02000810) |= BIT(0);		// Load the title at 2000838h
+	*(u32*)(0x02000810) |= BIT(1);		// Use colors 2000814h
+	*(u16*)(0x02000814) = 0x7FFF;		// Unlaunch Upper screen BG color (0..7FFFh)
+	*(u16*)(0x02000816) = 0x7FFF;		// Unlaunch Lower screen BG color (0..7FFFh)
+	memset((u8*)0x02000818, 0, 0x20+0x208+0x1C0);		// Unlaunch Reserved (zero)
+	int i2 = 0;
+	for (int i = 0; i < 14; i++) {
+		*(u8*)(0x02000838+i2) = hiyaNdsPath[i];		// Unlaunch Device:/Path/Filename.ext (16bit Unicode,end by 0000h)
+		i2 += 2;
+	}
+	while (*(u16*)(0x0200080E) == 0) {	// Keep running, so that CRC16 isn't 0
+		*(u16*)(0x0200080E) = swiCRC16(0xFFFF, (void*)0x02000810, 0x3F0);		// Unlaunch CRC16
+	}
+}
 
 void myIrqHandlerVBlank(void) {
 	if (language >= 0 && language < 6) {
@@ -44,6 +66,7 @@ void myIrqHandlerVBlank(void) {
 		softResetTimer = 0;
 	} else { 
 		if(softResetTimer == 60*2) {
+			unlaunchSetHiyaBoot();
 			memcpy((u32*)0x02000300,sr_data_srloader,0x020);
 			i2cWriteRegister(0x4a,0x70,0x01);
 			i2cWriteRegister(0x4a,0x11,0x01);	// Reboot into DSiMenu++
@@ -53,6 +76,7 @@ void myIrqHandlerVBlank(void) {
 
 	if(REG_KEYINPUT & (KEY_L | KEY_R | KEY_START | KEY_SELECT)) {
 	} else if (!gameSoftReset) {
+		unlaunchSetHiyaBoot();
     	memcpy((u32*)0x02000300,sr_data_srllastran,0x020);
     	i2cWriteRegister(0x4a,0x70,0x01);
     	i2cWriteRegister(0x4a,0x11,0x01);	// Reboot game
