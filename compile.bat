c3196a4fc5b147633a719a5917678bacf97aad1e
Epicpkmn11 <epicpkmn11@outlook.com>
2019-02-06 02:09:39 -0600

    Push some missing files

--- compile.bat
+++ compile.bat
@@ -14,22 +14,22 @@ copy "rungame.nds" "../7zfile/CFW - SDNAND root/title/00030015/534c524e/content/
 cd ..
 cd "slot1launch"
 make
-copy "slot1launch.nds" "../7zfile/_nds/dsimenuplusplus/slot1launch.srldr"
+copy "slot1launch.nds" "../7zfile/_nds/TWiLightMenu/slot1launch.srldr"
 cd ..
 cd "title"
 make
-copy "title.nds" "../7zfile/_nds/dsimenuplusplus/main.srldr"
+copy "title.nds" "../7zfile/_nds/TWiLightMenu/main.srldr"
 cd ..
 cd "settings"
 make
-copy "settings.nds" "../7zfile/_nds/dsimenuplusplus/settings.srldr"
+copy "settings.nds" "../7zfile/_nds/TWiLightMenu/settings.srldr"
 cd ..
 cd "romsel_dsimenutheme"
 make
-copy "romsel_dsimenutheme.nds" "../7zfile/_nds/dsimenuplusplus/dsimenu.srldr"
+copy "romsel_dsimenutheme.nds" "../7zfile/_nds/TWiLightMenu/dsimenu.srldr"
 cd ..
 cd "romsel_r4theme"
 make
-copy "romsel_r4theme.nds" "../7zfile/_nds/dsimenuplusplus/r4menu.srldr"
+copy "romsel_r4theme.nds" "../7zfile/_nds/TWiLightMenu/r4menu.srldr"
 cd ..
 pause
