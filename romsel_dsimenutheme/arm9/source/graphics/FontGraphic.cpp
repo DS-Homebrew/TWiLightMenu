a5ab3218141b213208e4bc7e5487901d23d80d66
RocketRobz <bobesh8@gmail.com>
2019-02-19 13:41:54 -0700

    Revert "Optimize character lookup in DSi Menu (#431)"
    This reverts commit ca2f1b0e1ab89145dd966c4a93466d65695dfacd.

--- romsel_dsimenutheme/arm9/source/graphics/FontGraphic.cpp
+++ romsel_dsimenutheme/arm9/source/graphics/FontGraphic.cpp
@@ -56,21 +56,9 @@ int FontGraphic::load(int textureID, glImage *_font_sprite,
  */
 unsigned int FontGraphic::getSpriteIndex(const u16 letter) {
 	unsigned int spriteIndex = 0;
-	unsigned int left = 0;
-	unsigned int right = imageCount;
-
-	while (left <= right)
-	{
-		unsigned int mid = left + ((right - left) / 2);
-		if (mapping[mid] == letter) {
-			spriteIndex = mid;
-			break;
-		}
-
-		if (mapping[mid] < letter) {
-			left = mid + 1;
-		} else {
-			right = mid - 1;
+	for (unsigned int i = 0; i < imageCount; i++) {
+		if (mapping[i] == letter) {
+			spriteIndex = i;
 		}
 	}
 	return spriteIndex;
