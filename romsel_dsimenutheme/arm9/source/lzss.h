445793933c67e77edf2e23f812fac6c88c5433c2
Ronny Chan <ronny6993@gmail.com>
2019-03-03 16:56:31 -0500

    Use mtheall's LZ77 function

--- romsel_dsimenutheme/arm9/source/lzss.h
+++ romsel_dsimenutheme/arm9/source/lzss.h
@@ -1,6 +1,12 @@
-#ifndef AS_DECOMPRESS_H
-#define AS_DECOMPRESS_H
+#ifndef LZ77_DECOMPRESS_H
+#define LZ77_DECOMPRESS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 void LZ77_Decompress(const unsigned char *src, unsigned char *dst);
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* DECOMPRESS_H */
