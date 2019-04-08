f332f62b0f7a8caea2bf03f9f9f6219de64eea22
Ronny Chan <ronny6993@gmail.com>
2019-04-08 02:53:23 -0400

    clarify comment in streamingaudio.h

--- romsel_dsimenutheme/arm9/source/streamingaudio.h
+++ romsel_dsimenutheme/arm9/source/streamingaudio.h
@@ -8,7 +8,7 @@
 
 
 #define FADE_STEPS 7                                           // Number of fill requests to fade out across when fade out is requested.
-#define STREAMING_BUF_LENGTH 96000                             // Size in samples (16 bits) => 256000B = 256KB * 2 = 512KB. // samples to keep cached
+#define STREAMING_BUF_LENGTH 96000                             // Size in samples (16 bits) => 96000 samples = 192KB * 2 buffers = 384KB RAM total.
 #define FILL_FACTOR 4                                          // The higher the fill factor the more frequent the fills will be requested.
 #define SAMPLES_PER_FILL (STREAMING_BUF_LENGTH >> FILL_FACTOR) // Samples to load into the fill buffer per fill.
 #define TOTAL_FILLS (1 << FILL_FACTOR)                         // Fills before we stop accepting new data into the fill buffer
