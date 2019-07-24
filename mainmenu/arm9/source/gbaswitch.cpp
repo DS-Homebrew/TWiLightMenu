9f14d470dd9d1ac15e51727cb49586a1987be8cf
RocketRobz <bobesh8@gmail.com>
2019-07-23 18:12:30 -0600

    gbaswitch: Clear VRAM A&B using DMA, instead of CPU

--- mainmenu/arm9/source/gbaswitch.cpp
+++ mainmenu/arm9/source/gbaswitch.cpp
@@ -21,10 +21,8 @@ void gbaSwitch(void) {
 		dmaCopy(gbaframePal, BG_PALETTE, 256*2);
 	} else {*/
 		// Clear VRAM A and B to show black border for GBA mode
-		for (u32 i = 0; i < 0x80000; i++) {
-			*(u32*)(0x06000000+i) = 0;
-			*(u32*)(0x06200000+i) = 0;
-		}
+		dmaFillHalfWords(0, (u16*)0x06000000, 0x80000);
+		dmaFillHalfWords(0, (u16*)0x06200000, 0x80000);
 	//}
 	// Switch to GBA mode
 	runNdsFile ("/_nds/TWiLightMenu/gbaswitch.srldr", 0, NULL, true, false, true, false, false);	
--- romsel_dsimenutheme/arm9/source/gbaswitch.cpp
+++ romsel_dsimenutheme/arm9/source/gbaswitch.cpp
@@ -31,13 +31,11 @@ void gbaSwitch(void) {
 	vramSetBankA(VRAM_A_MAIN_BG);
 	vramSetBankB(VRAM_B_MAIN_BG);
 	// Clear VRAM A and B to show black border for GBA mode
-	for (u32 i = 0; i < 0x80000; i++) {
-		*(u32*)(0x06000000+i) = 0;
-		*(u32*)(0x06200000+i) = 0;
-	}
-	/*if (gbaBorder == 1) {
-		loadGbaBorder("nitro:/gbaborder/nologo.bmp");
-	}*/
+	dmaFillHalfWords(0, (u16*)0x06000000, 0x80000);
+	dmaFillHalfWords(0, (u16*)0x06200000, 0x80000);
+	//if (gbaBorder == 1) {
+		//loadGbaBorder("nitro:/gbaborder.bmp");
+	//}
 	// Switch to GBA mode
 	runNdsFile ("/_nds/TWiLightMenu/gbaswitch.srldr", 0, NULL, true, false, true, false, false);	
 }
