6898a228eab14fc06022015132b98c284cc0d8de
Ronny Chan <ronny6993@gmail.com>
2019-07-27 18:50:27 -0400

    akmenu: fix memory bugs when making save file

--- romsel_aktheme/arm9/source/tool/stringtool.cpp
+++ romsel_aktheme/arm9/source/tool/stringtool.cpp
@@ -43,13 +43,14 @@ std::string formatString( const char* fmt, ... )
     return str;
 }
 
-std::string replaceAll(std::string str, const std::string &from, const std::string &to)
+std::string replaceAll(const std::string str, const std::string &from, const std::string &to)
 {
 	size_t start_pos = 0;
+    std::string newStr = std::string(str);
 	while ((start_pos = str.find(from, start_pos)) != std::string::npos)
 	{
-		str.replace(start_pos, from.length(), to);
+		newStr.replace(start_pos, from.length(), to);
 		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
 	}
-	return str;
+	return newStr;
 }
