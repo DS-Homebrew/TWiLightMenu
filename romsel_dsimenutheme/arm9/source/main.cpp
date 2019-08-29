58b2c10a6a4fee23603df838f033f34a9595af99
RocketRobz <bobesh8@gmail.com>
2019-08-29 08:37:08 -0600

    Add AP-patch for River City: Soccer Hooligans
    Added to SpeedBumpExclude list as well

--- romsel_dsimenutheme/arm9/source/main.cpp
+++ romsel_dsimenutheme/arm9/source/main.cpp
@@ -411,6 +411,7 @@ void SetSpeedBumpExclude(const char *filename) {
 		"B3R",	// Pokemon Ranger: Guardian Signs
 		"APU",	// Puyo Puyo!! 15th Anniversary
 		"BYO",	// Puyo Puyo 7
+	    "B3X",	// River City: Soccer Hooligans
 	    "ARZ",	// Rockman ZX/MegaMan ZX
 		"YZX",	// Rockman ZX Advent/MegaMan ZX Advent
 		"B6X",	// Rockman EXE: Operate Shooting Star
