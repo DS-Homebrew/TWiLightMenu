313776cd44461a9e6f9c09a38219f8f346d2b7d1
Ronny Chan <ronny6993@gmail.com>
2019-09-03 19:31:04 -0400

    akmenu: properly defer loading of system file names to fix themes not applying

--- romsel_aktheme/arm9/source/systemfilenames.cpp
+++ romsel_aktheme/arm9/source/systemfilenames.cpp
@@ -5,27 +5,29 @@
 #include "tool/stringtool.h"
 #include "common/dsimenusettings.h"
 
-SystemFilenames::SystemFilenames()
+void SystemFilenames::initFilenames()
 {
     std::string systemDirectory = formatString(SFN_SYSTEM_UI_DIRECTORY "%s", ms().ak_theme.c_str());
-    if (!sys().useNitroFS())
-    {
-        // nocashMessage("NNONITROFS");
-        // nocashMessage(systemDirectory.c_str());
-        _uiDirectory = systemDirectory;
-    }
-    else
-    {
-        if (access((systemDirectory + "/uisettings.ini").c_str(), F_OK) == 0)
+        if (!sys().useNitroFS())
         {
-            // nocashMessage("NITROFSOK");
-            // nocashMessage(systemDirectory.c_str());
+            nocashMessage("NNONITROFS");
+            nocashMessage(systemDirectory.c_str());
+            nocashMessage(ms().ak_theme.c_str());
             _uiDirectory = systemDirectory;
         }
         else
-        {  
-            cwl();
-            _uiDirectory = SFN_FALLBACK_UI_DIRECTORY;
+        {
+            if (access((systemDirectory + "/uisettings.ini").c_str(), F_OK) == 0)
+            {
+                nocashMessage("NITROFSOK");
+                nocashMessage(systemDirectory.c_str());
+                _uiDirectory = systemDirectory;
+            }
+            else
+            {  
+                cwl();
+                nocashMessage(SFN_FALLBACK_UI_DIRECTORY);
+                _uiDirectory = SFN_FALLBACK_UI_DIRECTORY;
+            }
         }
-    }
 }
