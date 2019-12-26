4b6e67d0ea0049bf46cf2adff1453f05d5a2b6c0
NightScript <maorninja@outlook.com>
2019-12-26 09:36:49 -0800

    Fix settings compilation error

--- settings/arm9/source/gbarunner2settings.cpp
+++ settings/arm9/source/gbarunner2settings.cpp
@@ -5,36 +5,33 @@
 
 GBAR2Settings::GBAR2Settings()
 {
-    useBottomScreen = false;
-    centerMask = true;
-    mainMemICache = true;
-    wramICache = true;
-    skipIntro = false;
+	useBottomScreen = false;
+	centerMask = true;
+	mainMemICache = true;
+	wramICache = true;
+	skipIntro = false;
 }
 
 void GBAR2Settings::loadSettings()
 {
-    easysave::ini gbarunner2ini(GBARUNNER2_INI);
+	easysave::ini gbarunner2ini(GBARUNNER2_INI);
 
-    // UI settings.
-   	useBottomScreen = (gbarunner2ini.GetString("emulation", "useBottomScreen", "false")=="false" ? false : true);
-   	centerMask = (gbarunner2ini.GetString("emulation", "centerMask", "true")=="true" ? true : false);
-   	mainMemICache = (gbarunner2ini.GetString("emulation", "mainMemICache", "true")=="true" ? true : false);
-   	wramICache = (gbarunner2ini.GetString("emulation", "wramICache", "true")=="true" ? true : false);
-   	skipIntro = (gbarunner2ini.GetString("emulation", "skipIntro", "false")=="false" ? false : true);
-
+	// UI settings.
+	useBottomScreen = (gbarunner2ini.GetString("emulation", "useBottomScreen", "false") == "false" ? false : true);
+	centerMask = (gbarunner2ini.GetString("emulation", "centerMask", "true") == "true" ? true : false);
+	mainMemICache = (gbarunner2ini.GetString("emulation", "mainMemICache", "true") == "true" ? true : false);
+	wramICache = (gbarunner2ini.GetString("emulation", "wramICache", "true") == "true" ? true : false);
+	skipIntro = (gbarunner2ini.GetString("emulation", "skipIntro", "false") == "false" ? false : true);
 }
 
 void GBAR2Settings::saveSettings()
 {
-	gbar2Fix = true;
-    easysave::ini gbarunner2ini(GBARUNNER2_INI);
+	easysave::ini gbarunner2ini(GBARUNNER2_INI);
 
-   	gbarunner2ini.SetString("emulation", "useBottomScreen", useBottomScreen ? "true" : "false");
-   	gbarunner2ini.SetString("emulation", "centerMask", centerMask ? "true" : "false");
-   	gbarunner2ini.SetString("emulation", "mainMemICache", mainMemICache ? "true" : "false");
-   	gbarunner2ini.SetString("emulation", "wramICache", wramICache ? "true" : "false");
-   	gbarunner2ini.SetString("emulation", "skipIntro", skipIntro ? "true" : "false");
-    gbarunner2ini.flush();
-	gbar2Fix = false;
+	gbarunner2ini.SetString("emulation", "useBottomScreen", useBottomScreen ? "true" : "false");
+	gbarunner2ini.SetString("emulation", "centerMask", centerMask ? "true" : "false");
+	gbarunner2ini.SetString("emulation", "mainMemICache", mainMemICache ? "true" : "false");
+	gbarunner2ini.SetString("emulation", "wramICache", wramICache ? "true" : "false");
+	gbarunner2ini.SetString("emulation", "skipIntro", skipIntro ? "true" : "false");
+	gbarunner2ini.flush();
 }
