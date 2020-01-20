42322b6be42845f6582e5d567037a57fcf99c061
RocketRobz <bobesh8@gmail.com>
2020-01-20 00:33:46 -0700

    Manual: Boost PNG rendering
    Co-Authored-By: NightScript <maorninja@outlook.com>

--- manual/arm9/source/errorScreen.cpp
+++ manual/arm9/source/errorScreen.cpp
@@ -27,18 +27,20 @@ static int timeTillChangeToNonExtendedImage = 0;
 static bool showNonExtendedImage = false;
 
 void loadSdRemovedImage(void) {
-	//FILE* file = fopen((arm7SCFGLocked ? "nitro:/graphics/sdRemovedSimple.bmp" : "nitro:/graphics/sdRemoved.bmp"), "rb");
-	std::vector<unsigned char> image[2];
-	unsigned width, height;
-	unsigned error = lodepng::decode(image[0], width, height, "nitro:/graphics/sdRemovedError.png");
-	if(error)	printSmallCentered(false, 30, "Error");
-	for(unsigned i=0;i<image[0].size()/4;i++) {
-  		sdRemovedExtendedImage[i] = image[0][i*4]>>3 | (image[0][(i*4)+1]>>3)<<5 | (image[0][(i*4)+2]>>3)<<10 | BIT(15);
+	uint imageWidth, imageHeight;
+	std::vector<unsigned char> image;
+
+	lodepng::decode(image, imageWidth, imageHeight, "nitro:/graphics/sdRemovedError.png");
+
+	for(uint i = 0; i < image.size(); i = i * 4) {
+		sdRemovedExtendedImage[i] = image[i] >> 3 | (image[i + 1] >> 3) << 5 | (image[i + 2] >> 3) << 10 | BIT(15);
 	}
 
-	lodepng::decode(image[1], width, height, "nitro:/graphics/sdRemoved.png");
-	for(unsigned i=0;i<image[1].size()/4;i++) {
-  		sdRemovedImage[i] = image[1][i*4]>>3 | (image[1][(i*4)+1]>>3)<<5 | (image[1][(i*4)+2]>>3)<<10 | BIT(15);
+	image.clear();
+	lodepng::decode(image, imageWidth, imageHeight, "nitro:/graphics/sdRemoved.png");
+
+	for(uint i = 0; i < image.size(); i = i * 4) {
+		sdRemovedImage[i] = image[i] >> 3 | (image[i + 1] >> 3) << 5 | (image[i + 2] >> 3) << 10 | BIT(15);
 	}
 }
 
