c7860a489ea61614d482982d8a703f25b36a5a96
RocketRobz <bobesh8@gmail.com>
2020-01-25 23:53:06 -0700

    Load GBA border (.png) for GBA mode

--- romsel_dsimenutheme/arm9/source/gbaswitch.cpp
+++ romsel_dsimenutheme/arm9/source/gbaswitch.cpp
@@ -1,41 +1,51 @@
+#include "graphics/ThemeTextures.h"
+#include "graphics/lodepng.h"
 #include "gbaswitch.h"
 #include "nds_loader_arm9.h"
 
-//int gbaBorder = 1;
+int gbaBorder = 1;
 
-/*void loadGbaBorder(const char* filename) {
-	FILE* file = fopen(filename, "rb");
+void loadGbaBorder(const char* filename) {
+	uint imageWidth, imageHeight;
+	std::vector<unsigned char> image;
 
-	if (file) {
-		// Start loading
-		fseek(file, 0xe, SEEK_SET);
-		u8 pixelStart = (u8)fgetc(file) + 0xe;
-		fseek(file, pixelStart, SEEK_SET);
-		for (int y=191; y>=0; y--) {
-			u16 buffer[256];
-			fread(buffer, 2, 0x100, file);
-			u16* src = buffer;
-			for (int i=0; i<256; i++) {
-				u16 val = *(src++);
-				VRAM_A[y*256+i] = ((val>>10)&0x1f) | ((val)&(0x1f<<5)) | (val&0x1f)<<10 | BIT(15);
-				VRAM_B[y*256+i] = ((val>>10)&0x1f) | ((val)&(0x1f<<5)) | (val&0x1f)<<10 | BIT(15);
-			}
-		}
+	lodepng::decode(image, imageWidth, imageHeight, filename);
+
+	for(uint i=0;i<image.size()/4;i++) {
+		tex().bmpImageBuffer()[i] = image[i*4]>>3 | (image[(i*4)+1]>>3)<<5 | (image[(i*4)+2]>>3)<<10 | BIT(15);
 	}
 
-	fclose(file);
-}*/
+    DC_FlushRange(tex().bmpImageBuffer(),SCREEN_WIDTH*SCREEN_HEIGHT*2);
+    dmaCopy(tex().bmpImageBuffer(),(void*)BG_BMP_RAM(0),SCREEN_WIDTH*SCREEN_HEIGHT*2);
+    dmaCopy(tex().bmpImageBuffer(),(void*)BG_BMP_RAM(8),SCREEN_WIDTH*SCREEN_HEIGHT*2);
+}
 
 void gbaSwitch(void) {
 	irqDisable(IRQ_VBLANK);
-	vramSetBankA(VRAM_A_MAIN_BG);
-	vramSetBankB(VRAM_B_MAIN_BG);
-	// Clear VRAM A and B to show black border for GBA mode
-	dmaFillHalfWords(0, (u16*)0x06000000, 0x80000);
-	dmaFillHalfWords(0, (u16*)0x06200000, 0x80000);
-	//if (gbaBorder == 1) {
-		//loadGbaBorder("nitro:/gbaborder.bmp");
-	//}
+
+  videoSetMode(MODE_5_2D | DISPLAY_BG3_ACTIVE);
+  videoSetModeSub(MODE_5_2D | DISPLAY_BG3_ACTIVE);
+
+	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
+	vramSetBankB(VRAM_B_MAIN_BG_0x06020000);
+	vramSetBankC(VRAM_C_SUB_BG_0x06200000);
+	vramSetBankD(VRAM_D_LCD);
+
+  // for the main screen
+  REG_BG3CNT = BG_BMP16_256x256 | BG_BMP_BASE(0) | BG_WRAP_OFF;
+  REG_BG3PA = 1 << 8; //scale x
+  REG_BG3PB = 0; //rotation x
+  REG_BG3PC = 0; //rotation y
+  REG_BG3PD = 1 << 8; //scale y
+  REG_BG3X = 0; //translation x
+  REG_BG3Y = 0; //translation y
+
+  memset((void*)BG_BMP_RAM(0),0,0x18000);
+  memset((void*)BG_BMP_RAM(8),0,0x18000);
+
+	if (gbaBorder == 1) {
+		loadGbaBorder("nitro:/graphics/gbaborder.png");
+	}
 	// Switch to GBA mode
 	runNdsFile ("/_nds/TWiLightMenu/gbaswitch.srldr", 0, NULL, true, false, true, false, false);	
 }
--- romsel_r4theme/arm9/source/gbaswitch.cpp
+++ romsel_r4theme/arm9/source/gbaswitch.cpp
@@ -1,29 +1,52 @@
+#include "graphics/lodepng.h"
 #include "gbaswitch.h"
 #include "nds_loader_arm9.h"
 
-//#include "gbaframe.h"
-//#include "gbaframe_srloader.h"
+extern u16 bmpImageBuffer[256*192];
 
-//int gbaBorder = 0;
+int gbaBorder = 1;
+
+void loadGbaBorder(const char* filename) {
+	uint imageWidth, imageHeight;
+	std::vector<unsigned char> image;
+
+	lodepng::decode(image, imageWidth, imageHeight, filename);
+
+	for(uint i=0;i<image.size()/4;i++) {
+		bmpImageBuffer[i] = image[i*4]>>3 | (image[(i*4)+1]>>3)<<5 | (image[(i*4)+2]>>3)<<10 | BIT(15);
+	}
+
+    DC_FlushRange(bmpImageBuffer,SCREEN_WIDTH*SCREEN_HEIGHT*2);
+    dmaCopy(bmpImageBuffer,(void*)BG_BMP_RAM(0),SCREEN_WIDTH*SCREEN_HEIGHT*2);
+    dmaCopy(bmpImageBuffer,(void*)BG_BMP_RAM(8),SCREEN_WIDTH*SCREEN_HEIGHT*2);
+}
 
 void gbaSwitch(void) {
-	vramSetBankA(VRAM_A_MAIN_BG);
-	vramSetBankB(VRAM_B_MAIN_BG);
-	/*if (gbaBorder == 2) {
-		int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+	irqDisable(IRQ_VBLANK);
 
-		dmaCopy(gbaframe_srloaderBitmap, bgGetGfxPtr(bg3), 256*256);
-		dmaCopy(gbaframe_srloaderPal, BG_PALETTE, 256*2);
-	} else if (gbaBorder == 1) {
-		int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+  videoSetMode(MODE_5_2D | DISPLAY_BG3_ACTIVE);
+  videoSetModeSub(MODE_5_2D | DISPLAY_BG3_ACTIVE);
 
-		dmaCopy(gbaframeBitmap, bgGetGfxPtr(bg3), 256*256);
-		dmaCopy(gbaframePal, BG_PALETTE, 256*2);
-	} else {*/
-		// Clear VRAM A and B to show black border for GBA mode
-		dmaFillHalfWords(0, (u16*)0x06000000, 0x80000);
-		dmaFillHalfWords(0, (u16*)0x06200000, 0x80000);
-	//}
+	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
+	vramSetBankB(VRAM_B_MAIN_BG_0x06020000);
+	vramSetBankC(VRAM_C_SUB_BG_0x06200000);
+	vramSetBankD(VRAM_D_LCD);
+
+  // for the main screen
+  REG_BG3CNT = BG_BMP16_256x256 | BG_BMP_BASE(0) | BG_WRAP_OFF;
+  REG_BG3PA = 1 << 8; //scale x
+  REG_BG3PB = 0; //rotation x
+  REG_BG3PC = 0; //rotation y
+  REG_BG3PD = 1 << 8; //scale y
+  REG_BG3X = 0; //translation x
+  REG_BG3Y = 0; //translation y
+
+  memset((void*)BG_BMP_RAM(0),0,0x18000);
+  memset((void*)BG_BMP_RAM(8),0,0x18000);
+
+	if (gbaBorder == 1) {
+		loadGbaBorder("nitro:/graphics/gbaborder.png");
+	}
 	// Switch to GBA mode
 	runNdsFile ("/_nds/TWiLightMenu/gbaswitch.srldr", 0, NULL, true, false, true, false, false);	
 }
