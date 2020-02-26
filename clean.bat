aca5696598453fbd5f59f301023e49ff7e510bc8
NightScript <maorninja@outlook.com>
2020-02-25 16:40:12 -0800

    Add in missing folders to clean.bat

--- clean.bat
+++ clean.bat
@@ -1,26 +1,47 @@
 @echo off
+
 cd "booter"
 make clean
 cd ..
+
 cd "booter_fc"
 make clean
 cd ..
-cd "rungame"
+
+cd "manual"
 make clean
 cd ..
-cd "slot1launch"
+
+cd "quickmenu"
 make clean
 cd ..
-cd "title"
+
+cd "romsel_aktheme"
 make clean
 cd ..
-cd "settings"
-make clean
-cd ..
+
 cd "romsel_dsimenutheme"
 make clean
 cd ..
+
 cd "romsel_r4theme"
 make clean
 cd ..
+
+cd "rungame"
+make clean
+cd ..
+
+cd "slot1launch"
+make clean
+cd ..
+
+cd "title"
+make clean
+cd ..
+
+cd "settings"
+make clean
+cd ..
+
 pause
