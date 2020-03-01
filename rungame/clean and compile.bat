1fcd87df78e835768fca16e3562dcd1ff60474da
RocketRobz <bobesh8@gmail.com>
2020-03-01 13:39:47 -0700

    Add `resetgame.srldr`

--- rungame/clean and compile.bat
+++ rungame/clean and compile.bat
@@ -1,4 +1,5 @@
 make clean
 make
 copy "rungame.nds" "../7zfile/DSi - CFW users/SDNAND root/title/00030015/534c524e/content/00000000.app"
+copy "rungame.nds" "../7zfile/_nds/TWiLightMenu/resetgame.srldr"
 pause
