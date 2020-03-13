878c26cb36ec05a64b773ea9c64f9f0293d8eaaf
RocketRobz <bobesh8@gmail.com>
2020-03-13 00:06:45 -0600

    Remove unused int

--- settings/arm9/source/common/flashcard.cpp
+++ settings/arm9/source/common/flashcard.cpp
@@ -9,17 +9,6 @@
 #include "common/systemdetails.h"
 #include "read_card.h"
 
-int flashcard;
-/* Flashcard value
-	0: DSTT/R4i Gold/R4i-SDHC/R4 SDHC Dual-Core/R4 SDHC Upgrade/SC DSONE
-	1: R4DS (Original Non-SDHC version)/ M3 Simply
-	2: R4iDSN/R4i Gold RTS/R4 Ultra
-	3: Acekard 2(i)/Galaxy Eagle/M3DS Real
-	4: Acekard RPG
-	5: Ace 3DS+/Gateway Blue Card/R4iTT
-	6: SuperCard DSTWO
-*/
-
 bool sdFound(void) {
 	if (access("sd:/", F_OK) == 0) {
 		return true;
