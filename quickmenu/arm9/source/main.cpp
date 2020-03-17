9cfbd6b03d350b7d1fec08f52f41dc9e52d6646b
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2020-03-17 17:45:33 +0800

    Derp Fix

--- quickmenu/arm9/source/main.cpp
+++ quickmenu/arm9/source/main.cpp
@@ -180,7 +180,7 @@ bool animateDsiIcons = false;
 int launcherApp = -1;
 int sysRegion = -1;
 
-//int guiLanguage = -1;
+int guiLanguage = -1;
 int bstrap_language = -1;
 bool boostCpu = false;	// false == NTR, true == TWL
 bool boostVram = false;
@@ -200,7 +200,7 @@ void LoadSettings(void) {
 	// Customizable UI settings.
 	colorMode = settingsini.GetInt("SRLOADER", "COLOR_MODE", 0);
 	blfLevel = settingsini.GetInt("SRLOADER", "BLUE_LIGHT_FILTER_LEVEL", 0);
-	//guiLanguage = settingsini.GetInt("SRLOADER", "LANGUAGE", -1);
+	guiLanguage = settingsini.GetInt("SRLOADER", "LANGUAGE", -1);
 	sdRemoveDetect = settingsini.GetInt("SRLOADER", "SD_REMOVE_DETECT", 1);
 	useGbarunner = settingsini.GetInt("SRLOADER", "USE_GBARUNNER2", 0);
 	if (!isRegularDS) useGbarunner = true;
