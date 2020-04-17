fede907f62cf0275b98f2acf98caf32801b1fd9b
RocketRobz <bobesh8@gmail.com>
2020-04-17 00:50:23 -0600

    Add more games to incompatible game list

--- universal/include/incompatibleGameMap.h
+++ universal/include/incompatibleGameMap.h
@@ -4,6 +4,9 @@
 static const char incompatibleGameList[][4] = {
 	"ANG", // Eigo ga Nigate na Otona no DS Training: Eigo Zuke
 	"BO5", // Golden Sun: Dark Dawn
+	"APD", // Pokemon Dash
+	"A24", // Pokemon Dash (Kiosk Demo)
+	"AH9", // Tony Hawk's American Sk8land
 };
 
 #endif // INCOMPATIBLEMAP_H
