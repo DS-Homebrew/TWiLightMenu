eb162120fbaf234693820f087c1fe33dd7674705
Pk11 <epicpkmn11@outlook.com>
2020-06-16 05:05:55 -0500

    Settings: Switch buttons to special symbols

--- settings/arm9/source/language.cpp
+++ settings/arm9/source/language.cpp
@@ -18,6 +18,53 @@ extern bool useTwlCfg;
 const char *languageIniPath;
 
 /**
+ * Get strings from the ini with special processing
+ */
+std::string getString(CIniFile &ini, const std::string &item, const std::string &defaultValue) {
+	std::string out = ini.GetString("LANGUAGE", item, defaultValue);
+
+	// Convert "\n" to actual newlines
+	for(uint i = 0; i < out.length() - 1; i++) {
+		if(out[i] == '\\') {
+			switch(out[i + 1]) {
+				case 'n':
+				case 'N':
+					out = out.substr(0, i) + '\n' + out.substr(i + 2);
+					break;
+				case 'a':
+				case 'A':
+					out = out.substr(0, i) + "" + out.substr(i + 2); // U+E000
+					break;
+				case 'b':
+				case 'B':
+					out = out.substr(0, i) + "" + out.substr(i + 2); // U+E001
+					break;
+				case 'x':
+				case 'X':
+					out = out.substr(0, i) + "" + out.substr(i + 2); // U+E002
+					break;
+				case 'y':
+				case 'Y':
+					out = out.substr(0, i) + "" + out.substr(i + 2); // U+E003
+					break;
+				case 'l':
+				case 'L':
+					out = out.substr(0, i) + "" + out.substr(i + 2); // U+E004
+					break;
+				case 'r':
+				case 'R':
+					out = out.substr(0, i) + "" + out.substr(i + 2); // U+E005
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	return out;
+}
+
+/**
  * Initialize translations.
  * Uses the language ID specified in settings.ui.language.
  */
@@ -52,7 +99,7 @@ void langInit(void)
 
 	CIniFile languageini(languageIniPath);
 
-#define STRING(what,def) STR_##what = languageini.GetString("LANGUAGE", ""#what, def);
+#define STRING(what,def) STR_##what = getString(languageini, ""#what, def);
 #include "language.inl"
 #undef STRING
 }
