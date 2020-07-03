a94a180f6b5306c4b3c0109c38e5b0a9c1cb42e0
Ronny Chan <ronny6993@gmail.com>
2020-07-03 18:49:44 -0400

    cheatwnd: remove writecheatstofile

--- romsel_aktheme/arm9/source/windows/cheatwnd.cpp
+++ romsel_aktheme/arm9/source/windows/cheatwnd.cpp
@@ -529,20 +529,3 @@ std::string CheatWnd::getCheats()
   std::replace( cheats.begin(), cheats.end(), '\n', ' ');
   return cheats;
 }
-
-void CheatWnd::writeCheatsToFile(std::string data, const char* path) {
-  std::fstream fs;
-  fs.open(path, std::ios::binary | std::fstream::out);
-  std::stringstream str;
-  u32 value;
-  while(1) {
-    str.clear();
-    str << data.substr(0, data.find(" "));
-    str >> std::hex >> value;
-    fs.write(reinterpret_cast<char*>(&value),sizeof(value));
-    data = data.substr(data.find(" ")+1);
-    if((int)data.find(" ") == -1) break;
-  }
-  fs.write("\0\0\0\xCF", 4);
-  fs.close();
-}
