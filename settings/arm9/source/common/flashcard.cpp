ce2cae6dad41e08f164c344e0ce84cad6faf2a38
RocketRobz <bobesh8@gmail.com>
2020-11-10 20:08:57 -0700

    Re-disable Slot-1, if twl_flashcardInit failed

--- settings/arm9/source/common/flashcard.cpp
+++ settings/arm9/source/common/flashcard.cpp
@@ -91,6 +91,10 @@ TWL_CODE void twl_flashcardInit(void) {
 			io_dldi_data = dldiLoadFromFile("nitro:/dldi/CycloIEvo.dldi");
 			fatMountSimple("fat", &io_dldi_data->ioInterface);
 		} */
+
+		if (!flashcardFound()) {
+			disableSlot1();
+		}
 	}
 }
 
