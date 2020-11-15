51e3c17f3d84f3a755d4e674403ec2cbb1227edd
RocketRobz <bobesh8@gmail.com>
2020-11-14 17:26:23 -0700

    Add *Over the Hedge* to heap shrink blacklist

--- universal/include/speedBumpExcludeMap.h
+++ universal/include/speedBumpExcludeMap.h
@@ -53,6 +53,7 @@
 		"YNP",	// Need for Speed: ProStreet
 		"A2D",	// New Super Mario Bros.
 		"BSK",	// Nine Hours, Nine Persons, Nine Doors
+		"AH5",	// Over the Hedge
 		"CUP",	// Peppa Pig: The Game
 		"A8N",	// Planet Puzzle League
 		"YFY",	// Pokemon Mystery Dungeon: Explorers of Darkness
