a39beca312c256100125fe99cb07fd9999958964
RocketRobz <bobesh8@gmail.com>
2021-01-09 11:55:06 -0700

    3DS themes: Try another tweak

--- romsel_dsimenutheme/arm9/source/graphics/ThemeTextures.cpp
+++ romsel_dsimenutheme/arm9/source/graphics/ThemeTextures.cpp
@@ -1428,8 +1428,8 @@ void loadRotatingCubes() {
 
 		if (doRead) {
 			if (rvidCompressed) {
-				fread((void*)0x02480000, 1, 0x200000, videoFrameFile);
-				LZ77_Decompress((u8*)0x02480000, (u8*)rotatingCubesLocation);
+				fread((void*)0x02500000, 1, 0x200000, videoFrameFile);
+				LZ77_Decompress((u8*)0x02500000, (u8*)rotatingCubesLocation);
 			} else {
 				fread(rotatingCubesLocation, 1, 0x700000, videoFrameFile);
 			}
