b77e958286311970a0edd6d227ae9d02c1dca94a
RocketRobz <bobesh8@gmail.com>
2021-06-29 17:01:01 -0600

    Update asyncReadExcludeMap.h

--- universal/include/asyncReadExcludeMap.h
+++ universal/include/asyncReadExcludeMap.h
@@ -9,9 +9,15 @@ static const char asyncReadExcludeList[][4] = {
 	"BEZ", // Inazuma Eleven 3: Bomb Blast
 	"BE8", // Inazuma Eleven 3: Lightning Bolt
 	"BOE", // Inazuma Eleven 3: Team Ogre Attacks!
+	"BKI", // The Legend of Zelda: Spirit Tracks (Keeps AP-fix in effect)
+	"Y7S", // The Legend of Zelda: Spirit Tracks (Demo)
 	"AWV", // Nervous Brickdown
 	"B2K", // Ni no Kuni: The Jet Black Mage
-	"B3R", // Pokemon Ranger: Guardian Signs
+	"IRB", // Pokemon Black Version (Keeps AP-fix in effect)
+	"IRA", // Pokemon White Version (Keeps AP-fix in effect)
+	"IRE", // Pokemon Black Version 2 (Keeps AP-fix in effect)
+	"IRD", // Pokemon White Version 2 (Keeps AP-fix in effect)
+	"B3R", // Pokemon Ranger: Guardian Signs (Keeps AP-fix in effect)
 	"A5F", // Professor Layton and the Curious Village
 	"Y49", // Professor Layton and the Curious Village (Demo)
 	"YLT", // Professor Layton and the Diabolical/Pandora's Box
