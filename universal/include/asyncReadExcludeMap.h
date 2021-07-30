e4bbd7b5f44d5083d59809561903d3c4a82a1932
RocketRobz <bobesh8@gmail.com>
2021-07-30 13:25:33 -0600

    Add *Mario vs. Donkey Kong: Mini-Land Mayhem!* to asynch card read blacklist

--- universal/include/asyncReadExcludeMap.h
+++ universal/include/asyncReadExcludeMap.h
@@ -17,6 +17,7 @@ static const char asyncReadExcludeList[][4] = {
 	"BOE", // Inazuma Eleven 3: Team Ogre Attacks!
 	"YLU", // Last Window: The Secret of Cape West
 	"AY9", // Mario & Sonic at the Olympic Games
+	"V2G", // Mario vs. Donkey Kong: Mini-Land Mayhem!
 	"BKI", // The Legend of Zelda: Spirit Tracks (Keeps AP-fix in effect)
 	"Y7S", // The Legend of Zelda: Spirit Tracks (Demo)
 	"AWV", // Nervous Brickdown
