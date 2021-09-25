799621b2beca5ea93baabeac8f283a721f36e029
RocketRobz <bobesh8@gmail.com>
2021-09-24 21:44:17 -0600

    Part 2 of fcc2d926

--- title/arm9/source/main.cpp
+++ title/arm9/source/main.cpp
@@ -624,7 +624,9 @@ void lastRunROM()
 				err = runNdsFile("fat:/_dstwo/autoboot.nds", 0, NULL, true, true, true, runNds_boostCpu, runNds_boostVram, -1);
 			} else if ((memcmp(io_dldi_data->friendlyName, "TTCARD", 6) == 0)
 					 || (memcmp(io_dldi_data->friendlyName, "DSTT", 4) == 0)
-					 || (memcmp(io_dldi_data->friendlyName, "DEMON", 5) == 0)) {
+					 || (memcmp(io_dldi_data->friendlyName, "DEMON", 5) == 0)
+					 || (memcmp(io_dldi_data->friendlyName, "DSONE", 5) == 0)
+					 || (memcmp(io_dldi_data->friendlyName, "M3DS DLDI", 9) == 0)) {
 				CIniFile fcrompathini("fat:/TTMenu/YSMenu.ini");
 				fcPath = replaceAll(ms().romPath[ms().previousUsedDevice], "fat:/", slashchar);
 				fcrompathini.SetString("YSMENU", "AUTO_BOOT", fcPath);
