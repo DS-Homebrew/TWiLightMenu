deb4848b7fce6dc1b4ff96535fa2b439fcca0e55
RocketRobz <bobesh8@gmail.com>
2021-12-23 22:23:34 -0700

    Fix finding `nds-bootstrap.ini` for B4DS mode setting

--- quickmenu/arm9/source/common/bootstrapsettings.cpp
+++ quickmenu/arm9/source/common/bootstrapsettings.cpp
@@ -2,6 +2,7 @@
 #include "myDSiMode.h"
 #include "common/inifile.h"
 #include "common/bootstrappaths.h"
+#include "flashcard.h"
 #include "bootstrapsettings.h"
 #include <string.h>
 
@@ -12,7 +13,7 @@ BootstrapSettings::BootstrapSettings()
 
 void BootstrapSettings::loadSettings()
 {
-    CIniFile bootstrapini(BOOTSTRAP_INI);
+    CIniFile bootstrapini(sdFound() ? "sd:/_nds/nds-bootstrap.ini" : "fat:/_nds/nds-bootstrap.ini");
 
 	if (dsiFeatures()) {
 		b4dsMode = bootstrapini.GetInt("NDS-BOOTSTRAP", "B4DS_MODE", b4dsMode);
