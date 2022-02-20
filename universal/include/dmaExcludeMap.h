326924bc30d850d23eb9a0ffddc157b64afc2c91
RocketRobz <bobesh8@gmail.com>
2022-02-20 11:12:21 -0700

    Remove more games from card read DMA blacklist

--- universal/include/dmaExcludeMap.h
+++ universal/include/dmaExcludeMap.h
@@ -4,18 +4,11 @@
 static const char cardReadDMAExcludeList[][4] = {
 	"TAM", // The Amazing Spider-Man
 	"AWD", // Diddy Kong Racing
-	"A3V", // Digimon World: Dawn
-	"A6R", // Digimon World: Dusk
 	"BO5", // Golden Sun: Dark Dawn
 	"Y8L", // Golden Sun: Dark Dawn (Demo)
 	"AJS", // Jump! Super Stars
 	"AJU", // Jump! Ultimate Stars
-	"AZE", // The Legend of Zelda: Phantom Hourglass
-	"Y4C", // The Legend of Zelda: Phantom Hourglass (Demo)
-	"AUG", // Need for Speed: Underground 2
-	"A2O", // Prey the Stars
 	"B8I", // Spider-Man: Edge of Time
-	"AST", // Star Wars: Episode III: Revenge of the Sith
 	"CTX", // Tropix
 	"CP3", // Viva Pinata
 };
