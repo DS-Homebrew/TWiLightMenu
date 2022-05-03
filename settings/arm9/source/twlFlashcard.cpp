f8897a6df0d12057b4c7565e2a5e0347947510a2
RocketRobz <bobesh8@gmail.com>
2022-05-02 22:18:22 -0600

    Run `cardInit()` regardless of `Slot-1 microSD access` setting

--- settings/arm9/source/twlFlashcard.cpp
+++ settings/arm9/source/twlFlashcard.cpp
@@ -25,14 +25,14 @@ TWL_CODE void ShowGameInfo(const char gameid[], const char gamename[]) {
 
 TWL_CODE void twl_flashcardInit(void) {
 	if (REG_SCFG_MC != 0x11 && !sys().arm7SCFGLocked()) {
+		// Reset Slot-1 to allow reading title name and ID
+		cardInit(false);
+
 		CIniFile settingsini( DSIMENUPP_INI );
 
 		if (settingsini.GetInt("SRLOADER", "SECONDARY_ACCESS", 0) == false) {
 			return;
 		}
-
-		// Reset Slot-1 to allow reading title name and ID
-		cardInit(false);
 
 		//char gamename[13];
 		//char gameid[5];
