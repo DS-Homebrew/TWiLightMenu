14a517fde92b76c546d0a35474090c45de0884ac
RocketRobz <bobesh8@gmail.com>
2022-05-16 11:22:05 -0600

    Check for *Drill Dozer* and *Pokemon Pinball* when enabling rumble
    Unsure if they work

--- universal/source/common/my_rumble.c
+++ universal/source/common/my_rumble.c
@@ -40,11 +40,10 @@ bool my_isRumbleInserted(void) {
 	// Then, check for 0x96 to see if it's a GBA game
 	if (GBA_HEADER.is96h == 0x96) {
 		//if it is a game, we check the game code
-		//to see if it is warioware twisted
-		if (	(GBA_HEADER.gamecode[0] == 'R') &&
-				(GBA_HEADER.gamecode[1] == 'Z') &&
-				(GBA_HEADER.gamecode[2] == 'W')
-			)
+		//to see if it is WarioWare Twisted, Drill Dozer, or Pokemon Pinball: Ruby & Sapphire
+		if (((GBA_HEADER.gamecode[0] == 'R') && (GBA_HEADER.gamecode[1] == 'Z') && (GBA_HEADER.gamecode[2] == 'W'))
+		  || ((GBA_HEADER.gamecode[0] == 'V') && (GBA_HEADER.gamecode[1] == '4') && (GBA_HEADER.gamecode[2] == '9'))
+		  || ((GBA_HEADER.gamecode[0] == 'V') && (GBA_HEADER.gamecode[1] == 'P') && (GBA_HEADER.gamecode[2] == 'P')))
 		{
 			rumbleType = MY_WARIOWARE;
 			WARIOWARE_ENABLE = 8;
