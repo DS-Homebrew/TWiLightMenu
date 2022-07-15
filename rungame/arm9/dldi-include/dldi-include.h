5a8741f416b1c98382d0d1ead2bcc680d7299aab
RocketRobz <bobesh8@gmail.com>
2022-07-14 23:51:24 -0600

    Delete unused files

--- rungame/arm9/dldi-include/dldi-include.h
+++
@@ -1,9 +0,0 @@
-#ifndef DLDIINCLUDE_H
-#define DLDIINCLUDE_H
-
-extern unsigned char r4idsn_sd_dldi[];
-//extern unsigned int r4idsn_sd_dldi_len;
-extern unsigned char ak2_sd_dldi[];
-//extern unsigned int ak2_sd_dldi_len;
-
-#endif //DLDIINCLUDE_H
