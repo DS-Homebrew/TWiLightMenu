ce8580099b18fe9fbc425513bc47ba3f3ce3ec51
RocketRobz <bobesh8@gmail.com>
2022-08-14 22:23:31 -0600

    Remove some games from TWL clock speed blacklist

--- universal/include/twlClockExcludeMap.h
+++ universal/include/twlClockExcludeMap.h
@@ -2,45 +2,20 @@
 #define TWLCLOCKEXCLUDEMAP_H
 
 static const char twlClockExcludeList[][4] = {
-	"YAA", // Arkanoid DS
 	"CRL", // Coraline
-	"CLP", // Club Penguin: Elite Penguin Force
-	"CY9", // Club Penguin: EPF: Herbert's Revenge
-	"ATD", // Clubhouse Games
 	"YGD", // Diary Girl
-	"AKY", // Etrian Odyssey
-	"YIK", // Etrian Odyssey II: Heroes of Lagaard
 	"YGX", // Grand Theft Auto: Chinatown Wars
-	"AWI", // Hotel Dusk: Room 215
 	"C6C", // Infinite Space
-	"YLU", // Last Window: The Secret of Cape West
 	"YJB", // LEGO Batman: The Videogame
-	"B6F", // LEGO Batman 2: DC Super Heroes
-	"CS5", // LEGO Battles
-	"TLJ", // LEGO Friends
 	"BLH", // LEGO Harry Potter: Years 1-4
-	"B83", // LEGO Harry Potter: Years 5-7
 	"YLJ", // LEGO Indiana Jones: The Original Adventures
-	"BLJ", // LEGO Indiana Jones 2: The Adventure Continues
-	"TCB", // LEGO Legends of Chima: Laval's Journey
-	"TLR", // LEGO The Lord of the Rings
-	"TLM", // LEGO Marvel Super Heroes: Universe in Peril
-	"BVY", // LEGO Ninjago: The Videogame
-	"BZD", // LEGO Pirates of the Caribbean: The Video Game
-	"AL7", // LEGO Star Wars II: The Original Trilogy
 	"YLG", // LEGO Star Wars: The Complete Saga
-	"BL9", // LEGO Star Wars III: The Clone Wars
-	"YNN", // Magician's Quest: Mysterious Times
 	"AY9", // Mario & Sonic at the Olympic Games
 	"BZP", // Peppa Pig: Theme Park Fun
-	"AQW", // Puzzle Quest: Challenge of the Warlords
-	"BRJ", // Radiant Historia
 	"ARF", // Rune Factory: A Fantasy Harvest Moon
 	"AN6", // Rune Factory 2: A Fantasy Harvest Moon
 	"BRF", // Rune Factory 3: A Fantasy Harvest Moon
 	"ASC", // Sonic Rush
-	"ASM", // Super Mario 64 DS
-	"COZ", // The Wizard of Oz: Beyond the Yellow Brick Road
 	"CY8", // Yu-Gi-Oh! 5D's Stardust Accelerator: World Championship 2009
 };
 
