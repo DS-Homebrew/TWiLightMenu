31e055740ad239a268e3d90b4c48b06dd2116fca
RocketRobz <bobesh8@gmail.com>
2022-09-02 17:35:17 -0600

    Add *Paul's Monster Adventure* to B4DS DSiWare whitelist

--- universal/include/compatibleDSiWareMap.h
+++ universal/include/compatibleDSiWareMap.h
@@ -1,7 +1,7 @@
 #ifndef COMPATIBLEDSIWAREMAP_H
 #define COMPATIBLEDSIWAREMAP_H
 
-// B4DS DSiWare Whitelist (Total: 93)
+// B4DS DSiWare Whitelist (Total: 94)
 static const char compatibleGameListB4DS[][5] = {
 	"KJU", // GO Series: 10 Second Run
 	"K95", // 1950s Lawn Mower Kids
@@ -73,6 +73,7 @@ static const char compatibleGameListB4DS[][5] = {
 	"KDR", // Mr. Driller: Drill Till You Drop
 	"K2D", // Nintendo DSi + Internet
 	"KSUE", // Number Battle
+	"KP9", // Paul's Monster Adventure
 	"KPJ", // Paul's Shooting Adventure
 	"KUS", // Paul's Shooting Adventure 2
 	"KAP", // Art Style: PiCTOBiTS
