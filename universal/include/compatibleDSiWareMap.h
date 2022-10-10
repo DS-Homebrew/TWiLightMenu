a236b80f1592df221043507a76f15fcafad1a88b
RocketRobz <bobesh8@gmail.com>
2022-10-10 14:09:23 -0600

    Add RAM limitation message for *Pinball Attack!*

--- universal/include/compatibleDSiWareMap.h
+++ universal/include/compatibleDSiWareMap.h
@@ -156,7 +156,7 @@ static const char compatibleGameListB4DS[][5] = {
 	"KZT", // Zuma's Revenge!
 };
 
-// B4DS DSiWare Whitelist (Show RAM limitation message) (Total: 11)
+// B4DS DSiWare Whitelist (Show RAM limitation message) (Total: 12)
 static const char compatibleGameListB4DSRAMLimited[][4] = {
 	"KAA", // Art Style: Aquia
 	"KFP", // Flipper
@@ -164,6 +164,7 @@ static const char compatibleGameListB4DSRAMLimited[][4] = {
 	"K6T", // Orion's Odyssey
 	"KE3", // PictureBook Games: The Royal Bluff
 	"KHR", // Picture Perfect: Pocket Stylist
+	"KPY", // GO Series: Pinball Attack!
 	"KS3", // Shantae: Risky's Revenge
 	"KZU", // Tales to Enjoy!: Little Red Riding Hood
 	"KZV", // Tales to Enjoy!: Puss in Boots
@@ -179,6 +180,7 @@ static int compatibleGameListB4DSRAMLimitedID[] = {
 	4, // Orion's Odyssey (Crashes at certain points)
 	1, // PictureBook Games: The Royal Bluff (No audio)
 	0, // Picture Perfect: Pocket Stylist (Parts of the game only)
+	4, // GO Series: Pinball Attack! (Crashes at certain points)
 	3, // Shantae: Risky's Revenge (Crashes at the Lighthouse after first fight)
 	4, // Tales to Enjoy!: Little Red Riding Hood (Crashes at certain points)
 	4, // Tales to Enjoy!: Puss in Boots (Crashes at certain points)
