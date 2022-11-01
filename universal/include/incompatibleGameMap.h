fb05a3621dd54d96070bcab45d8f90a3900f868f
RocketRobz <bobesh8@gmail.com>
2022-10-31 20:25:29 -0600

    Update incompatibleGameMap.h

--- universal/include/incompatibleGameMap.h
+++ universal/include/incompatibleGameMap.h
@@ -3,20 +3,16 @@
 
 static const char incompatibleGameListB4DS[][4] = {
 	"ADM", // Animal Crossing: Wild World
-	"AQC", // Crayon Shin-chan DS - Arashi o Yobu Nutte Crayoon Daisakusen!
 	"YRC", // Crayon Shin-chan - Arashi o Yobu Cinemaland Kachinko Gachinko Daikatsugeki!
 	"CL4", // Crayon Shin-Chan - Arashi o Yobu Nendororoon Daihenshin!
 	"BQB", // Crayon Shin-chan - Obaka Dainin Den - Susume! Kasukabe Ninja Tai!
 	"YKR", // Culdcept DS
 	"AWD", // Diddy Kong Racing
-	"AK4", // Kabu Trader Shun
 	"B7F", // The Magic School Bus: Oceans
 	"ARM", // Mario & Luigi: Partners in Time
 	"CLJ", // Mario & Luigi: Bowser's Inside Story
 	"COL", // Mario & Sonic at the Olympic Winter Games
 	"AMM", // Minna no Mahjong DS
-	"ARZ", // Rockman ZX/MegaMan ZX
-	"YZX", // Rockman ZX Advent/MegaMan ZX Advent
 	"B6Z", // Rockman Zero Collection/MegaMan Zero Collection
 	"CS3", // Sonic & Sega All-Stars Racing
 	"AH9", // Tony Hawk's American Sk8land
