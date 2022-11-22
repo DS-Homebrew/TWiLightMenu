ddd0da89306a03590a45d8112deff1f83ff53f6b
RocketRobz <bobesh8@gmail.com>
2022-11-21 23:40:19 -0700

    Add *Pop+ Solo* to B4DS DSiWare whitelist

--- universal/include/compatibleDSiWareMap.h
+++ universal/include/compatibleDSiWareMap.h
@@ -1,7 +1,7 @@
 #ifndef COMPATIBLEDSIWAREMAP_H
 #define COMPATIBLEDSIWAREMAP_H
 
-// B4DS DSiWare Whitelist (Total: 197)
+// B4DS DSiWare Whitelist (Total: 198)
 static const char compatibleGameListB4DS[][5] = {
 	"KJU", // GO Series: 10 Second Run
 	"K95", // 1950s Lawn Mower Kids
@@ -130,6 +130,7 @@ static const char compatibleGameListB4DS[][5] = {
 	"KZL", // Plants vs. Zombies
 	"KPP", // Pop Island
 	"KPF", // Pop Island: Paperfield
+	"KPI", // Pop+ Solo
 	"KOQ", // GO Series: Portable Shrine Wars
 	"KAK", // Art Style: precipice
 	"KPH", // Prehistorik Man
