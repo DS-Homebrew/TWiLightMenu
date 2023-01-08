074fa1bc779bf1d4a0bde16e36d2e5654af07264
RocketRobz <bobesh8@gmail.com>
2023-01-08 16:29:07 -0700

    Add *James Cameron's Avatar* to card read DMA blacklist

--- universal/include/dmaExcludeMap.h
+++ universal/include/dmaExcludeMap.h
@@ -7,6 +7,7 @@ static const char cardReadDMAExcludeList[][4] = {
 	"AWD", // Diddy Kong Racing
 	"BO5", // Golden Sun: Dark Dawn
 	"Y8L", // Golden Sun: Dark Dawn (Demo)
+	"VAV", // James Cameron's Avatar
 	"AJS", // Jump! Super Stars
 	"AJU", // Jump! Ultimate Stars
 	"B8I", // Spider-Man: Edge of Time
