5a66553d7d69b2f304368f78c63830af9787268b
RocketRobz <bobesh8@gmail.com>
2023-01-18 23:06:33 -0700

    Add *Artillery: Knights vs. Orcs* to B4DS DSiWare whitelist

--- universal/include/compatibleDSiWareMap.h
+++ universal/include/compatibleDSiWareMap.h
@@ -1,7 +1,7 @@
 #ifndef COMPATIBLEDSIWAREMAP_H
 #define COMPATIBLEDSIWAREMAP_H
 
-// B4DS DSiWare Whitelist (Total: 212)
+// B4DS DSiWare Whitelist (Total: 213)
 static const char compatibleGameListB4DS[][5] = {
 	"KJU", // GO Series: 10 Second Run
 	"K95", // 1950s Lawn Mower Kids
@@ -33,6 +33,7 @@ static const char compatibleGameListB4DS[][5] = {
 	"KSA", // Arcade Hoops Basketball
 	"KRD", // Armada
 	"KAY", // Army Defender
+	"K9Z", // Artillery: Knights vs. Orcs
 	"KSR", // Aura-Aura Climber
 	"KAD", // Art Style: BASE 10
 	"K8B", // Beauty Academy
