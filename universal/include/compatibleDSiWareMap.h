3c0f1bd76c609c7c795ce2ed0169f74b2d177d17
RocketRobz <bobesh8@gmail.com>
2023-02-01 21:49:50 -0700

    Add *5 in 1 Mahjong* to B4DS DSiWare whitelist

--- universal/include/compatibleDSiWareMap.h
+++ universal/include/compatibleDSiWareMap.h
@@ -1,7 +1,7 @@
 #ifndef COMPATIBLEDSIWAREMAP_H
 #define COMPATIBLEDSIWAREMAP_H
 
-// B4DS DSiWare Whitelist (Total: 235)
+// B4DS DSiWare Whitelist (Total: 236)
 static const char compatibleGameListB4DS[][5] = {
 	"KYP", // 1st Class Poker & BlackJack
 	"KJU", // GO Series: 10 Second Run
@@ -19,6 +19,7 @@ static const char compatibleGameListB4DS[][5] = {
 	"KTF", // 4 Travellers: Play French
 	"KTS", // 4 Travellers: Play Spanish
 	"K45", // 40-in-1: Explosive Megamix
+	"KRJ", // 5 in 1 Mahjong
 	"K2O", // 505 Tangram
 	"K99", // 99Bullets
 	"K9W", // 99Moves
