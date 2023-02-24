573c07bbf66b0bee34c82b06abc3a91aede5bca9
RocketRobz <bobesh8@gmail.com>
2023-02-23 18:25:37 -0700

    Add *Cosmo Fighters* to B4DS DSiWare whitelist

--- universal/include/compatibleDSiWareMap.h
+++ universal/include/compatibleDSiWareMap.h
@@ -1,7 +1,7 @@
 #ifndef COMPATIBLEDSIWAREMAP_H
 #define COMPATIBLEDSIWAREMAP_H
 
-// B4DS DSiWare Whitelist (Total: 280)
+// B4DS DSiWare Whitelist (Total: 281)
 static const char compatibleGameListB4DS[][5] = {
 	"KYP", // 1st Class Poker & BlackJack
 	"KJU", // GO Series: 10 Second Run
@@ -102,6 +102,7 @@ static const char compatibleGameListB4DS[][5] = {
 	"KTDT", "KTB", // Clubhouse Games Express: Strategy Pack
 	"KXF", // Color Commando
 	"KC7", // Commando: Steel Disaster
+	"KCX", // Cosmo Fighters
 	"KDC", // Crash-Course Domo
 	"K32", // CuteWitch! runner
 	"KF3", // Dairojo! Samurai Defenders
