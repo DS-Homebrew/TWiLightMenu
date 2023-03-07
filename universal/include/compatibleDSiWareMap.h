634c06ab801eddd61b19c45e0902ec8f70d2dbf1
RocketRobz <bobesh8@gmail.com>
2023-03-06 22:04:37 -0700

    Add *Keibadou Uma no Suke 2012* to B4DS DSiWare whitelist

--- universal/include/compatibleDSiWareMap.h
+++ universal/include/compatibleDSiWareMap.h
@@ -1,7 +1,7 @@
 #ifndef COMPATIBLEDSIWAREMAP_H
 #define COMPATIBLEDSIWAREMAP_H
 
-// B4DS DSiWare Whitelist (Total: 320)
+// B4DS DSiWare Whitelist (Total: 321)
 static const char compatibleGameListB4DS[][5] = {
 	"KYP", // 1st Class Poker & BlackJack
 	"KJU", // GO Series: 10 Second Run
@@ -193,6 +193,7 @@ static const char compatibleGameListB4DS[][5] = {
 	"KD5", // Jinia Supasonaru: Waei Rakubiki Jiten
 	"KJF", // Just SING! 80's
 	"K3H", // Kazu De Asobu: Mahoujin To Imeji Kei-san
+	"KUX", // Keibadou Uma no Suke 2012
 	"KT9", // Kung Fu Dragon
 	"KQ9", // The Legend of Zelda: Four Swords: Anniversary Edition
 	"KLD", // Legendary Wars: T-Rex Rumble
