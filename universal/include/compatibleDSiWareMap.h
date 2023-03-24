a0aa9db5f0ba542cfac091ffe8271a2435e2116f
RocketRobz <bobesh8@gmail.com>
2023-03-23 21:02:30 -0600

    Add *Super Yum Yum: Puzzle Adventures* to B4DS-Debug DSiWare whitelist

--- universal/include/compatibleDSiWareMap.h
+++ universal/include/compatibleDSiWareMap.h
@@ -544,7 +544,7 @@ static int compatibleGameListB4DSAllRAMLimitedID[] = {
 	2, // Yummy Yummy Cooking Jam (No music)
 };
 
-// B4DS DSiWare Whitelist (DS Debug consoles with 8MB of RAM) (Total: 30)
+// B4DS DSiWare Whitelist (DS Debug consoles with 8MB of RAM) (Total: 31)
 static const char compatibleGameListB4DSDebug[][5] = {
 	// Not counted towards total
 	"KBEV", // Bejeweled Twist (Europe, Australia)
@@ -579,6 +579,7 @@ static const char compatibleGameListB4DSDebug[][5] = {
 	"KSJ", // Shawn Johnson Gymnastics
 	"KYS", // Spot the Difference
 	"K4W", // Super Swap
+	"K4P", // Super Yum Yum: Puzzle Adventures
 	"KDZ", // Trajectile / Reflect Missile
 };
 
