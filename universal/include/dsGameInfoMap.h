b273ec6d77626d922267939ca42bed94672ab479
RocketRobz <bobesh8@gmail.com>
2023-10-18 21:57:41 -0600

    Update `sidewaysGameList`

--- universal/include/dsGameInfoMap.h
+++ universal/include/dsGameInfoMap.h
@@ -4,6 +4,9 @@
 // Games that start sideways
 static const char sidewaysGameList[][4] = {
 	"KAD", // Art Style: BASE 10
+	"AND", // Brain Age: Train Your Brain in Minutes a Day!
+	"ANM", // Brain Age 2: More Training in Minutes a Day!
+	"YCU", // Crosswords DS
 	"DHS", // Picture Perfect Hair Salon
 	"A8N", // Planet Puzzle League
 	"AZL", // Style Savvy
