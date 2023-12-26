2b47061c2bc9e9700dd68acf2cccc4a2328a6b04
RocketRobz <bobesh8@gmail.com>
2023-12-26 14:27:14 -0700

    Derp fix

--- universal/include/dmaExcludeMap.h
+++ universal/include/dmaExcludeMap.h
@@ -7,10 +7,10 @@ static const char cardReadDMAExcludeList[][4] = {
 	"AWD", // Diddy Kong Racing
 	"BO5", // Golden Sun: Dark Dawn
 	"Y8L", // Golden Sun: Dark Dawn (Demo)
+	"VIJ", // iCarly 2: iJoin the Click!
 	"VAV", // James Cameron's Avatar
 	"AJS", // Jump! Super Stars
 	"AJU", // Jump! Ultimate Stars
-	"VIJ", // iCarly 2: iJoin the Click!
 	"VCK", // My Healthy Cooking Coach: Easy Way to Cook Healthy
 	"B8I", // Spider-Man: Edge of Time
 	"AFZ", // Transformets: Autobots
