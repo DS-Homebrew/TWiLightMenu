07021967d6050895425b87baf6605b073e6a6e0a
RocketRobz <bobesh8@gmail.com>
2023-12-26 14:23:03 -0700

    Update dmaExcludeMap.h

--- universal/include/dmaExcludeMap.h
+++ universal/include/dmaExcludeMap.h
@@ -10,7 +10,7 @@ static const char cardReadDMAExcludeList[][4] = {
 	"VAV", // James Cameron's Avatar
 	"AJS", // Jump! Super Stars
 	"AJU", // Jump! Ultimate Stars
-	"VIJ", // iCarly 2: iJoin The Click!
+	"VIJ", // iCarly 2: iJoin the Click!
 	"VCK", // My Healthy Cooking Coach: Easy Way to Cook Healthy
 	"B8I", // Spider-Man: Edge of Time
 	"AFZ", // Transformets: Autobots
