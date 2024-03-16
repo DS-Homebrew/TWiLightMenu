a6bceddd128dc2fe4c6f012aae14b26b29f7863f
RocketRobz <bobesh8@gmail.com>
2024-03-15 19:36:13 -0600

    Derp fix

--- romsel_dsimenutheme/arm9/source/fileBrowse.cpp
+++ romsel_dsimenutheme/arm9/source/fileBrowse.cpp
@@ -342,7 +342,7 @@ void getDirectoryContents(std::vector<DirEntry> &dirContents, const std::vector<
 						fread(segaString, 1, 4, mdFile);
 						fclose(mdFile);
 
-						if (!((segaEntryPointU8[0] == 0) && ((strcmp(segaString, "SEGA") == 0) || ((segaEntryPoint >= 8) && (segaEntryPoint < 0x3FFFFF))))) {
+						if (!((segaEntryPointReversed[0] == 0) && ((strcmp(segaString, "SEGA") == 0) || ((segaEntryPoint >= 8) && (segaEntryPoint < 0x3FFFFF))))) {
 							// Invalid string or entry point found
 							continue;
 						}
