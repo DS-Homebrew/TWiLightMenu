f35fcf133fd2a0c302b33784596cd33a6d1a7199
RocketRobz <bobesh8@gmail.com>
2024-09-09 23:49:55 -0600

    Improve retail/homebrew detection by first checking for retail, before assuming homebrew
    Closes #1753

--- romsel_r4theme/arm9/source/iconTitle.cpp
+++ romsel_r4theme/arm9/source/iconTitle.cpp
@@ -909,8 +909,8 @@ void getGameInfo(bool isDir, const char* name)
 	toncset(gameTid, 0, 4);
 	isTwlm = false;
 	isDSiWare = false;
-	isHomebrew = false;
-	isModernHomebrew = false;
+	isHomebrew = true;
+	isModernHomebrew = true;
 	requiresRamDisk = false;
 	requiresDonorRom = false;
 	infoFound = false;
@@ -1143,43 +1143,53 @@ void getGameInfo(bool isDir, const char* name)
 
 		fseek(fp, ndsHeader.arm9romOffset + ndsHeader.arm9executeAddress - ndsHeader.arm9destination, SEEK_SET);
 		fread(arm9StartSig, sizeof(u32), 4, fp);
-		if (arm9StartSig[0] == 0xE3A00301
-		 && arm9StartSig[1] == 0xE5800208
-		 && arm9StartSig[2] == 0xE3A00013
-		 && arm9StartSig[3] == 0xE129F000) {
-			isHomebrew = true;
-			isModernHomebrew = true; // Homebrew is recent (supports reading from SD without a DLDI driver)
-			if (ndsHeader.arm7executeAddress >= 0x037F0000 && ndsHeader.arm7destination >= 0x037F0000) {
-				if ((ndsHeader.arm9binarySize == 0xC9F68 && ndsHeader.arm7binarySize == 0x12814)	// Colors! v1.1
-				|| (ndsHeader.arm9binarySize == 0x1B0864 && ndsHeader.arm7binarySize == 0xDB50)	// Mario Paint Composer DS v2 (Bullet Bill)
-				|| (ndsHeader.arm9binarySize == 0xE78FC && ndsHeader.arm7binarySize == 0xF068)		// SnowBros v2.2
-				|| (ndsHeader.arm9binarySize == 0xD45C0 && ndsHeader.arm7binarySize == 0x2B7C)		// ikuReader v0.058
-				|| (ndsHeader.arm9binarySize == 0x7A124 && ndsHeader.arm7binarySize == 0xEED0)		// PPSEDS r11
-				|| (ndsHeader.arm9binarySize == 0x54620 && ndsHeader.arm7binarySize == 0x1538)		// XRoar 0.24fp3
-				|| (ndsHeader.arm9binarySize == 0x2C9A8 && ndsHeader.arm7binarySize == 0xFB98)		// NitroGrafx v0.7
-				|| (ndsHeader.arm9binarySize == 0x22AE4 && ndsHeader.arm7binarySize == 0xA764)) {	// It's 1975 and this man is about to show you the future
-					isModernHomebrew = false; // Have nds-bootstrap load it (in case if it doesn't)
-				}
+		if (arm9StartSig[0] == 0xE3A0C301
+		 && arm9StartSig[1] == 0xE58CC208) {
+			// Title seems to be developed with Nintendo SDK, verify
+			if ((arm9StartSig[2] >= 0xEB000000 && arm9StartSig[2] < 0xEC000000) // SDK 2 & TWL SDK 5
+			 && (arm9StartSig[3] >= 0xE3A00000 && arm9StartSig[3] < 0xE3A01000)) {
+				isHomebrew = false;
+				isModernHomebrew = false;
+			} else
+			if (arm9StartSig[2] == 0xE1DC00B6 // SDK 3-5
+			 && arm9StartSig[3] == 0xE3500000) {
+				isHomebrew = false;
+				isModernHomebrew = false;
 			}
-		} else if ((memcmp(ndsHeader.gameTitle, "NDS.TinyFB", 10) == 0)
-				 || (memcmp(ndsHeader.gameTitle, "MAGIC FLOOR", 11) == 0)
-				 || (memcmp(ndsHeader.gameTitle, "UNLAUNCH.DSI", 12) == 0)) {
-			isHomebrew = true;
-			isModernHomebrew = true; // No need to use nds-bootstrap
-		} else if ((memcmp(ndsHeader.gameTitle, "NMP4BOOT", 8) == 0)
-		 || (ndsHeader.arm7executeAddress >= 0x037F0000 && ndsHeader.arm7destination >= 0x037F0000)) {
-			isHomebrew = true; // Homebrew is old (requires a DLDI driver to read from SD)
-		} else if (ndsHeader.unitCode != 0 && (ndsHeader.accessControl & BIT(4))) {
-			isDSiWare = true; // Is a DSiWare game
 		}
 
-		if (isHomebrew && !ms().secondaryDevice) {
-			if ((ndsHeader.arm9binarySize == 0x98F70 && ndsHeader.arm7binarySize == 0xED94)		// jEnesisDS 0.7.4
-			|| (ndsHeader.arm9binarySize == 0x48950 && ndsHeader.arm7binarySize == 0x74C4)			// SNEmulDS06-WIP2
-			|| (ndsHeader.arm9binarySize == 0xD45C0 && ndsHeader.arm7binarySize == 0x2B7C)			// ikuReader v0.058
-			|| (ndsHeader.arm9binarySize == 0x54620 && ndsHeader.arm7binarySize == 0x1538)) {		// XRoar 0.24fp3
-				requiresRamDisk = true;
+		if (isHomebrew) {
+			if (arm9StartSig[0] == 0xE3A00301
+			 && arm9StartSig[1] == 0xE5800208
+			 && arm9StartSig[2] == 0xE3A00013
+			 && arm9StartSig[3] == 0xE129F000) {
+				// isModernHomebrew = true; // Homebrew is recent (supports reading from SD without a DLDI driver)
+				if (ndsHeader.arm7executeAddress >= 0x037F0000 && ndsHeader.arm7destination >= 0x037F0000) {
+					if ((ndsHeader.arm9binarySize == 0xC9F68 && ndsHeader.arm7binarySize == 0x12814)	// Colors! v1.1
+					|| (ndsHeader.arm9binarySize == 0x1B0864 && ndsHeader.arm7binarySize == 0xDB50)	// Mario Paint Composer DS v2 (Bullet Bill)
+					|| (ndsHeader.arm9binarySize == 0xE78FC && ndsHeader.arm7binarySize == 0xF068)		// SnowBros v2.2
+					|| (ndsHeader.arm9binarySize == 0xD45C0 && ndsHeader.arm7binarySize == 0x2B7C)		// ikuReader v0.058
+					|| (ndsHeader.arm9binarySize == 0x7A124 && ndsHeader.arm7binarySize == 0xEED0)		// PPSEDS r11
+					|| (ndsHeader.arm9binarySize == 0x54620 && ndsHeader.arm7binarySize == 0x1538)		// XRoar 0.24fp3
+					|| (ndsHeader.arm9binarySize == 0x2C9A8 && ndsHeader.arm7binarySize == 0xFB98)		// NitroGrafx v0.7
+					|| (ndsHeader.arm9binarySize == 0x22AE4 && ndsHeader.arm7binarySize == 0xA764)) {	// It's 1975 and this man is about to show you the future
+						isModernHomebrew = false; // Have nds-bootstrap load it (in case if it doesn't)
+					}
+				}
+			} else if ((ndsHeader.unitCode == 0) && ((memcmp(ndsHeader.gameTitle, "NMP4BOOT", 8) == 0)
+			 || (ndsHeader.arm7executeAddress >= 0x037F0000 && ndsHeader.arm7destination >= 0x037F0000))) {
+				isModernHomebrew = false; // Homebrew is old (requires a DLDI driver to read from SD)
 			}
+			if (!ms().secondaryDevice) {
+				if ((ndsHeader.arm9binarySize == 0x98F70 && ndsHeader.arm7binarySize == 0xED94)		// jEnesisDS 0.7.4
+				|| (ndsHeader.arm9binarySize == 0x48950 && ndsHeader.arm7binarySize == 0x74C4)			// SNEmulDS06-WIP2
+				|| (ndsHeader.arm9binarySize == 0xD45C0 && ndsHeader.arm7binarySize == 0x2B7C)			// ikuReader v0.058
+				|| (ndsHeader.arm9binarySize == 0x54620 && ndsHeader.arm7binarySize == 0x1538)) {		// XRoar 0.24fp3
+					requiresRamDisk = true;
+				}
+			}
+		} else if (ndsHeader.unitCode != 0 && (ndsHeader.accessControl & BIT(4))) {
+			isDSiWare = true; // Is a DSiWare game
 		}
 
 		if (!isHomebrew) {
