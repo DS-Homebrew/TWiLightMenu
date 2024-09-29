9ce71163b1b96245b79466d856661cbf000c5094
RocketRobz <bobesh8@gmail.com>
2024-09-28 22:54:18 -0600

    Slot-1: SCFG setting no longer overrides SD access setting

--- booter_fc/arm7/source/main.c
+++ booter_fc/arm7/source/main.c
@@ -70,9 +70,15 @@ void powerButtonCB() {
 //---------------------------------------------------------------------------------
 int main() {
 //---------------------------------------------------------------------------------
-	REG_SCFG_ROM = 0x101;
-	REG_SCFG_CLK = (BIT(0) | BIT(1) | BIT(2) | BIT(7) | BIT(8));
-	REG_SCFG_EXT = 0x93FFFB06;
+	{
+		REG_SCFG_ROM = 0x101;
+		REG_SCFG_CLK = (BIT(0) | BIT(1) | BIT(2) | BIT(7) | BIT(8));
+		const bool sdAccess = (REG_SCFG_EXT & BIT(18));
+		REG_SCFG_EXT = 0x93FFFB06;
+		if (!sdAccess) {
+			REG_SCFG_EXT &= ~BIT(18);
+		}
+	}
 	*(vu16*)(0x04004012) = 0x1988;
 	*(vu16*)(0x04004014) = 0x264C;
 	*(vu16*)(0x04004C02) = 0x4000;	// enable powerbutton irq (Fix for Unlaunch 1.3)
