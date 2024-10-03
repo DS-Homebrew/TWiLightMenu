0fd717f53029b747df366941771b3f7d17c115c7
RocketRobz <bobesh8@gmail.com>
2024-10-03 16:54:44 -0600

    Update make cia.bat

--- booter/make cia.bat
+++ booter/make cia.bat
@@ -1,6 +1,4 @@
 @echo off
 make_cia --srl="booter.nds"
-make_cia --srl="booter_rungame.nds"
 copy "booter.cia" "../7zfile/3DS - CFW users/cia/TWiLight Menu.cia"
-copy "booter_rungame.cia" "../7zfile/3DS - CFW users/cia/TWiLight Menu - Game booter.cia"
 pause
