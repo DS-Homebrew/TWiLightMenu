1a336e4d4bac24462b8586b09c64c571f711dd83
RocketRobz <bobesh8@gmail.com>
2026-07-01 21:58:20 -0500

    Revert "Add *Soma Bringer* to card read DMA blacklist"
    This reverts commit f9b5fd080f78b7e938e9f25b91563c8159bbec4d.

--- universal/include/dmaExcludeMap.h
+++ universal/include/dmaExcludeMap.h
@@ -9,7 +9,6 @@ static const char cardReadDMAExcludeList[][4] = {
 	"Y8L", // Golden Sun: Dark Dawn (Demo)
 	"VIJ", // iCarly 2: iJoin the Click!
 	"VAV", // James Cameron's Avatar
-	"YBS", // Soma Bringer
 	"YWS", // Sonic Chronicles: The Dark Brotherhood
 	"B8I", // Spider-Man: Edge of Time
 	"AFZ", // Transformers: Autobots
