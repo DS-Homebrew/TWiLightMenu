459182886ade3eacec59cb6aa24e3098d766c795
RocketRobz <bobesh8@gmail.com>
2026-08-19 22:33:20 -0500

    gbaswitch: Further reduce color banding of GBA borders

--- romsel_dsimenutheme/arm9/source/gbaswitch.cpp
+++ romsel_dsimenutheme/arm9/source/gbaswitch.cpp
@@ -10,61 +10,41 @@ void loadGbaBorder(const char* filename) {
 	std::vector<unsigned char> image;
 	lodepng::decode(image, imageWidth, imageHeight, filename);
 	bool alternatePixel = false;
+	bool alternatePixel2 = false;
 
 	u16* bmpImageBuffer = new u16[256 * 192];
 
-	for (uint i = 0; i < image.size()/4; i++) {
-		image[(i*4)+3] = 0;
-		if (alternatePixel) {
-			if (image[(i*4)] >= 0x4 && image[(i*4)] < 0xFC) {
-				image[(i*4)] += 0x4;
-				image[(i*4)+3] |= BIT(0);
+	for (int b = 0; b < 2; b++) {
+		for (uint i = 0; i < image.size()/4; i++) {
+			const u8 oldR = image[(i*4)];
+			const u8 oldG = image[(i*4)+1];
+			const u8 oldB = image[(i*4)+2];
+			u8 newR = oldR;
+			u8 newG = oldG;
+			u8 newB = oldB;
+			if (alternatePixel) {
+				if (oldR >= 4 && oldR < 0xFC) newR += 4;
+				if (oldG >= 4 && oldG < 0xFC) newG += 4;
+				if (oldB >= 4 && oldB < 0xFC) newB += 4;
 			}
-			if (image[(i*4)+1] >= 0x4 && image[(i*4)+1] < 0xFC) {
-				image[(i*4)+1] += 0x4;
-				image[(i*4)+3] |= BIT(1);
+			if (alternatePixel2) {
+				if (((oldR/2) % 2) == 1 && newR < 0xFE) newR += 2;
+				if (((oldG/2) % 2) == 1 && newG < 0xFE) newG += 2;
+				if (((oldB/2) % 2) == 1 && newB < 0xFE) newB += 2;
 			}
-			if (image[(i*4)+2] >= 0x4 && image[(i*4)+2] < 0xFC) {
-				image[(i*4)+2] += 0x4;
-				image[(i*4)+3] |= BIT(2);
+			bmpImageBuffer[i] = newR>>3 | (newG>>3)<<5 | (newB>>3)<<10 | BIT(15);
+			if ((i % 256) == 255) {
+				alternatePixel = !alternatePixel;
+				alternatePixel2 = !alternatePixel2;
 			}
+			alternatePixel = !alternatePixel;
+			alternatePixel2 = !alternatePixel2;
 		}
-		bmpImageBuffer[i] = image[i*4]>>3 | (image[(i*4)+1]>>3)<<5 | (image[(i*4)+2]>>3)<<10 | BIT(15);
-		if ((i % 256) == 255) alternatePixel = !alternatePixel;
 		alternatePixel = !alternatePixel;
-	}
-	DC_FlushRange(bmpImageBuffer,SCREEN_WIDTH*SCREEN_HEIGHT*2);
-	dmaCopy(bmpImageBuffer,(void*)BG_BMP_RAM(0),SCREEN_WIDTH*SCREEN_HEIGHT*2);
 
-	alternatePixel = false;
-	for (uint i = 0; i < image.size()/4; i++) {
-		if (alternatePixel) {
-			if (image[(i*4)+3] & BIT(0)) {
-				image[(i*4)] -= 0x4;
-			}
-			if (image[(i*4)+3] & BIT(1)) {
-				image[(i*4)+1] -= 0x4;
-			}
-			if (image[(i*4)+3] & BIT(2)) {
-				image[(i*4)+2] -= 0x4;
-			}
-		} else {
-			if (image[(i*4)] >= 0x4 && image[(i*4)] < 0xFC) {
-				image[(i*4)] += 0x4;
-			}
-			if (image[(i*4)+1] >= 0x4 && image[(i*4)+1] < 0xFC) {
-				image[(i*4)+1] += 0x4;
-			}
-			if (image[(i*4)+2] >= 0x4 && image[(i*4)+2] < 0xFC) {
-				image[(i*4)+2] += 0x4;
-			}
-		}
-		bmpImageBuffer[i] = image[i*4]>>3 | (image[(i*4)+1]>>3)<<5 | (image[(i*4)+2]>>3)<<10 | BIT(15);
-		if ((i % 256) == 255) alternatePixel = !alternatePixel;
-		alternatePixel = !alternatePixel;
+		DC_FlushRange(bmpImageBuffer,SCREEN_WIDTH*SCREEN_HEIGHT*2);
+		dmaCopy(bmpImageBuffer,(void*)BG_BMP_RAM(b==1 ? 8 : 0),SCREEN_WIDTH*SCREEN_HEIGHT*2);
 	}
-	DC_FlushRange(bmpImageBuffer,SCREEN_WIDTH*SCREEN_HEIGHT*2);
-	dmaCopy(bmpImageBuffer,(void*)BG_BMP_RAM(8),SCREEN_WIDTH*SCREEN_HEIGHT*2);
 
 	delete[] bmpImageBuffer;
 }
